6220386e5af7837d8766952eaeeea07470c973bf
ujo4eva <92157639+ujo4eva@users.noreply.github.com>
2024-01-16 18:13:07 +0100

    Did a small program in class today

---
+++ classroom/perimeter_of_rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   
+    int length = 20;
+    int height = 9;
+
+    int perimeter = 2*(20+9);
+
+    printf("The Perimeter of a rectangle given length %d and height %d is %d.\n", length, height, perimeter);
+
+    return 0;
+}
