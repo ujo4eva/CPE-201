ea90948cad2fbc269367c7ffc6123c74457503e9
ujo4eva <92157639+ujo4eva@users.noreply.github.com>
2024-01-19 14:57:06 +0100

    building...

---
+++ practice/calculator/calculator v2.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+void main() {
+    double numbers[100];
+    char operations[100];
+    int num_count = 0;
+    int op_count = 0;
+    char input_string[100];
+    int decimal_places = 4;
+
+    while (true) {
+
+        printf("Enter numbers and operations (separate them with spaces please): ");
+
+        fgets(input_string, 100, stdin);
+
+        char *token = strtok(input_string, " ");
+        while (token != NULL) {
+
+            if(isdigit(*token)) {
+                numbers[num_count++] = atof(token);
+            } else {
+                operations[op_count++] = *token;
+                }
+
+        token = strtok(NULL, " ");
+
+        }
+
+    }
+
+        for (int i = 0; i < op_count; i++) {
+
+            if (operations[i] != '+' &&operations[i] != '-' &&operations[i] != '*' &&operations[i] != '/') {
+                printf("Invalid Operation: %c\n", operations[i]);
+                return;
+            } 
+        }
+
+        if (num_count - op_count !=1) {
+
+            printf("Invalid Input, unbalanced numbers and operations\n");
+            return;
+        }
+
+        for (int i = 0; i < op_count; i++) {
+            if (operations[i] == '/' &&numbers[i + 1] == 0) {
+                printf("Them dey divide by zero for your village abi?\n");
+                return;
+            }
+        }
+        
+        double result = numbers[0];
+        for (int i = 0; i < op_count; i++) {
+            switch (operations[i]) {
+            case
+
+            '+':
+
+            result += numbers[i + 1];
+            break;
+
+            case
+
+            '-':
+
+            result -= numbers[i + 1];
+            break;
+
+            case
+
+            '*':
+
+            result *= numbers[i + 1];
+            break;
+
+            case
+
+            '/':
+
+            result *= numbers[i + 1];
+            break;
+
+            }
+        }
+
+        printf("Result: %.*lf", decimal_places, result);
+    
+
+    return;
+
+}
