e752d7eeef6584cdb41b3ea5f235c007a4278f92
ujo4eva <92157639+ujo4eva@users.noreply.github.com>
2024-01-20 14:20:20 +0100

    .

--- practice/calculator/calculator v2.c
+++ practice/calculator/calculator v2.c
@@ -2,4 +2,25 @@
 #include <math.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
+int get_numbers_and_operations (double numbers[], char operations[], int *num_count, int *op_count);
+
+    char input_string[100];
+    printf("Enter numbers and operations (separate with spaces): ");
+    fgets(input_string, 100, stdin);
+
+    char *token = strtok(input_string, " ");
+    while (token = ! NULL) {
+        if(isdigit(*token)) {
+            numbers[*num_count] = atof(token);
+            (*num_count)++;
+        } else {
+            operations[*op_count] = (token);
+            (*op_count)++;
+        }
+
+        token = strtok(NULL, "");
+
+    return 0;
+    }
