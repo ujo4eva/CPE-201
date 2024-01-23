611d467c242ce4e5573cc0f5183ec410463200f0
ujo4eva <92157639+ujo4eva@users.noreply.github.com>
2024-01-23 15:50:58 +0100

    this man don vex me

---
+++ classroom/23-01-2024/vowel_or_consonant/vowel_printer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX_SIZE 50
+
+int main() {
+    char userInput[MAX_SIZE];
+
+    int count_vowel = 0;
+    
+    printf("Enter any word:\n");
+    
+    fgets(userInput, sizeof(userInput), stdin);
+
+    //loop through all the characters entered by the user
+
+    for(int i=0; i < MAX_SIZE; i++) {
+        if(userInput[i]=="a" || userInput[i]=="0" || userInput[i]=="u") {
+            count_vowel ++;
+            printf("%s\t", userInput[i]);
+        }
+    }
+
+    printf("\n"),
+
+    printf("Total number of vowels in %s is: %d.\n", userInput, count_vowel);
+
+    return 0;
+
+}
