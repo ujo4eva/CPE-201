4db7e7c8d95c20ec3898f5098cfe64dd2cdcfed9
ujo4eva <92157639+ujo4eva@users.noreply.github.com>
2024-01-25 18:55:33 +0100

    program that takes in an array of numbers and spits them out in reverse completed. Have an idea to improve it but cba for now at least

---
+++ Assignment/reverse_number_array/reverse_number_array.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void reverse(int *num_array, int start, int end) {
+    if (start >= end) {
+        return;
+    }
+    
+    int temp = num_array[start];
+    num_array[start] = num_array[end];
+    num_array[end] = temp;
+
+    reverse(num_array, start + 1, end - 1);
+
+    return;
+}
+
+int main() {
+    int num_array[100];
+    int num_elements;
+
+    printf("How many numbers are in your array? (max. 100): ");
+    scanf("%d", &num_elements);
+
+    if (num_elements > 100) {
+        printf("You entered too many numbers genius.");
+        return 1; //indicate an error
+    }
+
+    printf("Enter your chosen numbers: ", num_elements);
+    for (int i = 0; i < num_elements; i++) {
+        scanf("%d", &num_array[i]);
+    }
+
+    reverse(num_array, 0, num_elements - 1);
+    printf("Reversed array: "); {
+        for (int i = 0; i < num_elements; i++)
+    printf("%d ", num_array[i]);
+    }
+
+    printf("\n:)\n");
+
+    return;
+
+}
